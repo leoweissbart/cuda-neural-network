12259dae22193e17a41a7d3d6a5fb89cc45b2a19
Léo Weissbart <leo.weissbart@gmail.com>
2021-03-17 17:16:44 +0100

    makefile changes

--- Debug/src/layers/subdir.mk
+++ Debug/src/layers/subdir.mk
@@ -23,8 +23,8 @@ CU_DEPS += \
 src/layers/%.o: ../src/layers/%.cu
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
-	/usr/local/cuda-10.0/bin/nvcc -I/usr/local/cuda-10.0/include -G -g -O0 -ccbin aarch64-linux-gnu-g++ -gencode arch=compute_53,code=sm_53 -m64 -odir "src/layers" -M -o "$(@:%.o=%.d)" "$<"
-	/usr/local/cuda-10.0/bin/nvcc -I/usr/local/cuda-10.0/include -G -g -O0 -m64 -ccbin aarch64-linux-gnu-g++   "$@" "$<"
+	/usr/local/cuda-10.0/bin/nvcc -I/usr/local/cuda-10.0/include -G -g -O0 -v -ccbin aarch64-linux-gnu-g++ -gencode arch=compute_53,code=sm_53 -m64 -odir "src/layers" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-10.0/bin/nvcc -I/usr/local/cuda-10.0/include -G -g -O0 -v -m64 -ccbin aarch64-linux-gnu-g++   -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
