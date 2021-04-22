d10c0d7a4f82c5dd44401b81943cb248c547cd1d
Léo Weissbart <leo.weissbart@gmail.com>
2021-04-22 10:35:04 +0200

    clean makfile

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /usr/local/cuda-10.0/samples/common/lib/linux/aarch64/libGLEW.a
-
-LIBS := -lglut
-
